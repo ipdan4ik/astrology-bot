3fbb5e95ac3231ef9f64782e3970522e3f59bbb7
ipu <uwu@ipu.moe>
2026-05-20 16:44:43 +0300

    feat: bot UI callback-data factories

---
+++ src/quantuum/bot/ui/callbacks.py
@@ -0,0 +1,21 @@
+from aiogram.filters.callback_data import CallbackData
+
+
+class ProfileCb(CallbackData, prefix="prof"):
+    action: str  # edit
+    field: str = ""
+
+
+class HistoryCb(CallbackData, prefix="hist"):
+    action: str  # page | open
+    page: int = 0
+    bp_id: int = 0
+
+
+class BlueprintCb(CallbackData, prefix="bp"):
+    action: str  # download | preview | back
+    bp_id: int
+
+
+class OnboardCb(CallbackData, prefix="onb"):
+    action: str  # start | cancel
